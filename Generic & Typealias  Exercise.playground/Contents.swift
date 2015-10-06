a4113b1f70d92a88d84dbffc3d61546018f30673
Osiris <noirozr@icloud.com>
2015-10-06 18:32:55 +0800

    remain to be down

--- Generic & Typealias  Exercise.playground/Contents.swift
+++ Generic & Typealias  Exercise.playground/Contents.swift
@@ -1,9 +1,30 @@
-//: Playground - noun: a place where people can play
+//: Written by Osiris
+//: この番组は御覧のスポンサーの提供で送りします
 
 import UIKit
 
-class Character<T> {
+struct Stack<T> {
+    var items = [T]()
     
+    mutating func push(item: T) {
+        items.append(item)
+    }
+    
+    mutating func pop() -> T {
+        return items.removeLast()
+    }
 }
+
+var st1 = Stack<Int>()
+st1.push(1)
+st1.push(2)
+
+print(st1.pop())
+
+
+
+
+
+
 
 
